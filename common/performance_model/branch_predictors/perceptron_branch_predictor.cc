1658afc194dce4622f513e30367ae1da00cf867c
lou296A <loualsteens@gmail.com>
2024-02-17 18:29:07 +0800

    perceptron coded and compile to be tested and debug

---
+++ common/performance_model/branch_predictors/perceptron_branch_predictor.cc
@@ -0,0 +1,69 @@
+// this is perceptron branch predictor
+
+#include "simulator.h"
+#include "perceptron_branch_predictor.h"
+
+PerceptronBranchPredictor::PerceptronBranchPredictor(String name, core_id_t core_id, UInt32 size)
+	: BranchPredictor(name, core_id)
+
+{
+	// perceptronTable=  new int[size][HIST_LEN]; 
+	yout = 0;
+	// printf("size %u\n", size); 
+	for(int  i=0; i < TABLE_SIZE ; i++){
+		for(int j=0; j < HIST_LEN; j++){
+			perceptronTable[i][j] = 0;
+		}
+	}
+}
+
+PerceptronBranchPredictor::~PerceptronBranchPredictor()
+{
+}
+
+bool PerceptronBranchPredictor::predict(bool indirect, IntPtr ip, IntPtr target)
+{
+	//reset the prediction
+	int perceptronIndex = ip % TABLE_SIZE;
+	int prediction = 0;
+	prediction = perceptronTable[perceptronIndex][0];
+	for(int i=1; i < HIST_LEN + 1; i++){
+		if(ghr[i - 1] == 1){
+			prediction += perceptronTable[perceptronIndex][i];   
+		}
+		else{
+			prediction -= perceptronTable[perceptronIndex][i]; 
+		}
+	}
+	yout = abs(prediction); 
+
+	if(prediction >= 0)
+		return 1; 
+	return 0; 
+
+}
+
+void PerceptronBranchPredictor::update(bool predicted, bool actual, bool indirect, IntPtr ip, IntPtr target)
+{
+	// for performance analysis
+	updateCounters(predicted, actual);       
+	//update the perceptron weight
+	int t ;
+	if(actual == 0){
+		t = -1; 
+	}
+	else {
+		t = 1; 
+	}
+	int perceptronIndex = ip % TABLE_SIZE; 	
+	if((predicted != actual) || yout <= threshold){
+		perceptronTable[perceptronIndex][0] = perceptronTable[perceptronIndex][0] + t;
+		for(int i = 1; i < HIST_LEN + 1; i++){
+			perceptronTable[perceptronIndex][i] = perceptronTable[perceptronIndex][i] + t  * ghr[i-1];
+		}
+	}	
+	// update the global history table
+	ghr = (ghr << 1);
+	ghr.set(0, t); 
+	
+}
---
+++ common/performance_model/branch_predictors/perceptron_branch_predictor.h
@@ -0,0 +1,25 @@
+#ifndef PERCEPTRON_BRANCH_PREDICTOR_H
+#define PERCEPTRON_BRANCH_PREDICTOR_H
+
+#include "branch_predictor.h"
+#include <bitset>
+#include <vector>
+#define HIST_LEN   59 
+#define threshold 127
+#define TABLE_SIZE 1024
+class PerceptronBranchPredictor : public BranchPredictor
+{
+	public:
+		PerceptronBranchPredictor(String name, core_id_t core_id, UInt32 size);
+		~PerceptronBranchPredictor();
+
+		bool predict(bool indirect, IntPtr ip, IntPtr target);
+		void update(bool predicted, bool actual, bool indirect, IntPtr ip, IntPtr target);
+
+	private:
+		int yout;
+		std::bitset<HIST_LEN> ghr;   
+		int perceptronTable[TABLE_SIZE][HIST_LEN+1];
+};
+
+#endif
